61f30ee4d9f9f99bf2fdf2314b23f9e4a9aa9fa4
ashwin6thstreet <ashwin.bm@6thstreet.com>
2020-12-01 19:14:45 +0530

    updating

--- src/main/java/testRunner/CucumberRunner.java
+++ src/main/java/testRunner/CucumberRunner.java
@@ -4,7 +4,7 @@ package testRunner;
 import base.CucumberBase;
 import cucumber.api.CucumberOptions;
 
-@CucumberOptions(strict = true, monochrome = true, features = "src/test/resources/feature_backup/StoreCredit.feature", 
+@CucumberOptions(strict = true, monochrome = true, features = "src/test/resources/features/PaymentPage.feature", 
 glue = "stepdefinition", format = {	"pretty","json:target/cucumber.json"},// tags = { "@StoreCreditAndMagento" }, 
 plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-extent-reports/extent-report.html" }, dryRun = false)
 
