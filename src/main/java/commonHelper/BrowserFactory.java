3d02207fd7992917d644ce8288f27babeb318d6d
prasanna22062020 <prasanna.kumar@6thstreet.com>
2021-01-08 11:04:17 +0530

    Removed set binary

--- src/main/java/commonHelper/BrowserFactory.java
+++ src/main/java/commonHelper/BrowserFactory.java
@@ -19,15 +19,14 @@ public class BrowserFactory {
 	ThreadLocal<String> loggedinUserEmail = new ThreadLocal<String>();
 	ThreadLocal<String> environment = new ThreadLocal<String>();
 
-
 	private Logger log = Logger.getLogger(BrowserFactory.class.getName());
 
-	/**Singleton constructor declared**/
+	/** Singleton constructor declared **/
 	private BrowserFactory() {
 
 	}
 
-	/**Creating instance of class**/
+	/** Creating instance of class **/
 	public static BrowserFactory getInstance() {
 		new BrowserFactory().log.info("Creating Browser Factory Instance");
 		if (instance == null) {
@@ -36,7 +35,7 @@ public class BrowserFactory {
 		return instance;
 	}
 
-	/**Setting value of driver instance**/
+	/** Setting value of driver instance **/
 	public final void setDriver(String browser) {
 		if (browser.equalsIgnoreCase("firefox")) {
 			log.info("Creating Firefox Driver Instance");
@@ -45,118 +44,117 @@ public class BrowserFactory {
 			log.info("Creating Chrome Driver Instance");
 			ChromeOptions options = new ChromeOptions();
 			String chromeDriverPath = System.getProperty("user.dir") + "/src/test/resources/drivers/linux/chromedriver";
-				options.addArguments("--no-sandbox");
-				options.setBinary(chromeDriverPath);
-				options.addArguments("--disable-dev-shm-usage"); 
-				options.addArguments("start-maximized");
-		        options.addArguments("--disable-features=VizDisplayCompositor");
-		        options.addArguments("enable-automation");
-		        //options.addArguments("--headless");
-		        options.addArguments("--window-size=1920,1080");
-		        options.addArguments("--disable-gpu");
-		        options.addArguments("disable-infobars");
-		        options.addArguments("--disable-extensions");
-		        options.addArguments("--dns-prefetch-disable");
-		        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+			options.addArguments("--no-sandbox");
+			// options.setBinary(chromeDriverPath);
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("start-maximized");
+			options.addArguments("--disable-features=VizDisplayCompositor");
+			options.addArguments("enable-automation");
+			options.addArguments("--headless");
+			options.addArguments("--window-size=1920,1080");
+			options.addArguments("--disable-gpu");
+			options.addArguments("disable-infobars");
+			options.addArguments("--disable-extensions");
+			options.addArguments("--dns-prefetch-disable");
+			options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
 
-		        options.addArguments("enable-features=NetworkServiceInProcess");
+			options.addArguments("enable-features=NetworkServiceInProcess");
 
-		        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-		        capabilities.setCapability("marionette", true);
-		        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+			capabilities.setCapability("marionette", true);
+			capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 
-		        webDriver.set(new ChromeDriver(capabilities));
+			webDriver.set(new ChromeDriver(capabilities));
 		}
 	}
 
-	/**returning value of driver instance**/
+	/** returning value of driver instance **/
 	public WebDriver getDriver() {
 		log.info("Returning Driver Instance");
 		return webDriver.get();
 	}
 
-	/**Setting value of browser instance**/
+	/** Setting value of browser instance **/
 	public final void setBrowser(String browser) {
 		log.info("Creating Browser Instance " + browser);
 		webBrowser.set(browser);
 	}
 
-	/**returning value of browser instance**/
+	/** returning value of browser instance **/
 	public String getBrowser() {
 		log.info("Returning Browser Instance");
 		return webBrowser.get();
 	}
 
-	/**Setting value of country instance**/
+	/** Setting value of country instance **/
 	public final void setCountry(String countrycode) {
 		log.info("Setting Execution Country " + countrycode);
 		country.set(countrycode);
 	}
 
-	/**Setting value of language instance**/
+	/** Setting value of language instance **/
 	public final void setLanguage(String languagecode) {
 		log.info("Setting Execution language " + languagecode);
 		language.set(languagecode);
 	}
 
-	/**returning value of country instance**/
+	/** returning value of country instance **/
 	public String getCountry() {
 		log.info("Getting Execution Country");
 		return country.get();
 	}
 
-	/**returning value of language instance**/
+	/** returning value of language instance **/
 	public String getLanguage() {
 		log.info("Getting Execution language");
 		return language.get();
 	}
-	
-	/**returning value of order instance**/
+
+	/** returning value of order instance **/
 	public String getOrderNumber() {
 		log.info("Returning order Instance");
 		return ordernumber.get();
 	}
-	
-	/**Setting value of order instance**/
+
+	/** Setting value of order instance **/
 	public final void setOrder(String order) {
 		log.info("Setting Return Order instance" + order);
 		ordernumber.set(order);
 	}
-	
-	/**returning value of return instance**/
+
+	/** returning value of return instance **/
 	public String getReturnNumber() {
 		log.info("Returning returnOrder Instance");
 		return returnnumber.get();
 	}
-	
-	/**Setting value of return instance**/
+
+	/** Setting value of return instance **/
 	public final void setReturnNumber(String returnID) {
 		log.info("Setting Return Order instance " + returnID);
 		returnnumber.set(returnID);
 	}
-	
-	/**returning value of login User Email instance**/
+
+	/** returning value of login User Email instance **/
 	public String getLoggedInUserEmail() {
 		log.info("Returning login Email Instance");
 		return loggedinUserEmail.get();
 	}
-	
-	/**Setting value of login User Email instance**/
+
+	/** Setting value of login User Email instance **/
 	public final void setLoggedInUserEmail(String loginEmail) {
 		log.info("Setting Return login Email instance" + loginEmail);
 		loggedinUserEmail.set(loginEmail);
 	}
-	
-	/**Setting value of environment instance**/
+
+	/** Setting value of environment instance **/
 	public final void setEnvironment(String env) {
 		log.info("Creating environment Instance " + env);
 		environment.set(env);
 	}
 
-	/**returning value of environment instance**/
+	/** returning value of environment instance **/
 	public String getEnvironment() {
 		log.info("Returning environment Instance");
 		return environment.get();
 	}
 }
-
