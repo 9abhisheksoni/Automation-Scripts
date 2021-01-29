05529b99a8f70ac7f4d2d39fea6792362d813de9
prasanna22062020 <prasanna.kumar@6thstreet.com>
2021-01-29 11:20:46 +0530

    Including Jsom

--- src/main/java/testRunner/CucumberRunner.java
+++ src/main/java/testRunner/CucumberRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.CucumberOptions;
 
 @CucumberOptions(strict = true, monochrome = true, features = "src/test/resources/feature_backup/Cart.feature", 
 glue = "stepdefinition", format = {	"pretty"}, tags = { "@Regression" }, 
-plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-extent-reports/extent-report.html" }, dryRun = false)
+plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-extent-reports/extent-report.html","json:target/cucumber.json"}, dryRun = false)
 
 
 
