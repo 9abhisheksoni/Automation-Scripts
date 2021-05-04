34aee3dd636a67804e54526c0fad5c3d0bd1b78a
unknown <Abhishek Soni>
2021-05-04 21:25:23 +0530

    Revert "Working test set for Jenkins"
    This reverts commit 100758eec73e597f5008e83639ac285eba882e95.

--- src/main/java/testRunner/CucumberRunner.java
+++ src/main/java/testRunner/CucumberRunner.java
@@ -3,8 +3,8 @@ package testRunner;
 import base.CucumberBase;
 import cucumber.api.CucumberOptions;
 
-@CucumberOptions(strict = true, monochrome = true, features = "src/test/resources/feature_backup/SearchResultsPage.feature", glue = "stepdefinition", format = {
-		"pretty" }, tags = {"@Test1"}, plugin = {
+@CucumberOptions(strict = true, monochrome = true, features = "src/test/resources/feature_backup/Demo.feature", glue = "stepdefinition", format = {
+		"pretty" }, tags = {}, plugin = {
 				"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-extent-reports/extent-report.html",
 				"json:target/cucumber.json" }, dryRun = false)
 public class CucumberRunner extends CucumberBase {
